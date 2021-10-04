8dcd129a0daa8020ece2c414d68d9b7d8b9994bb
robot <robot@robot.robot>
2021-10-04 20:44:32 +0300

    a

--- robot code/main.py
+++ robot code/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import multiprocessing as mp
 import Camera as cam
 
--- testing files/sertest.py
+++ testing files/sertest.py
@@ -1,6 +1,6 @@
 import serial
 import struct
-from serial.tools import list_ports
+import math
 from time import sleep
 
 def send_ms(speeds): #unpack motorspeeds
@@ -11,8 +11,6 @@ def send_motorspeeds(m1 = 0,m2 = 0,m3 = 0,thrower = 0): # send speeds to motors 
     size = struct.calcsize('<hhhH')
     data = ser.read(size)
     values = struct.unpack('<hhhH',data)
-    for i in values:
-        print(i)
     return values #returns motor data
 
     #linear_velocity = overall_speed * math.cos(direction - math.radians(wheel_angle))
@@ -20,9 +18,9 @@ def send_motorspeeds(m1 = 0,m2 = 0,m3 = 0,thrower = 0): # send speeds to motors 
 def combine_moves(move1, p1, move2, p2):
     speed = [0,0,0,0]
     for i,v in enumerate(move1):
-        ret[i]+= v*p1
+        speed[i]+= v*p1
     for i,v in enumerate(move2):
-        ret[i]+= v*p2
+        speed[i]+= v*p2
     return speed
 
 def move_forward(speed): #same as move3
@@ -37,15 +35,34 @@ def move_2(speed):
 def move_1(speed):
     return [0, -speed, speed, 0]
 
+def thrower(speed):
+    return [0,0,0,speed]
+
+def move_omni(speed, angle): 
+    #linear_velocity = overall_speed * math.cos(direction - math.radians(wheel_angle))
+    
+    return [int(speed * math.cos(math.radians(angle) - math.radians(210))), int(speed * math.cos(math.radians(angle) - math.radians(300))), int(speed * math.cos(math.radians(angle) - math.radians(90))), 0]
+
+def rotate_omni(speed): 
+    return[speed,speed,speed,0]
+
+def rectify_speed(object, max_speed): 
+    mx = (max(object[0:len(object)-1]))
+    mx = max_speed/mx
+    object = [int(element * mx) for element in object]
+    return object
+
 #port =list(list_ports.comports())[0]
 #print(port.name)
+ser = None
 try:
     port='/dev/ttyACM0'
     ser = serial.Serial(port, baudrate=115200, timeout=3)
-    for i in range (2):
+    for i in range (10):
         print('----')
-        send_ms(combine_moves(move_2(10), 2,move_3(10) , 1))
+        send_ms(rectify_speed(combine_moves(move_omni(20, 0),1,rotate_omni(10),1),20))
         sleep(0.05)
 except Exception as e:
     print(e)
-ser.close()
+if ser != None:
+    ser.close()
