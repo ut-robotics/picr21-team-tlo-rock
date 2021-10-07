c4bf1bf6f124cffe3f294c6d0cc21f4301cbb13e
robot <robot@robot.robot>
2021-10-07 19:35:06 +0300

    movement

---
+++ robot code/movement_controller.py
@@ -0,0 +1,50 @@
+import serial
+import struct
+import math
+from time import sleep
+
+def send_ms(speeds): #unpack motorspeeds
+    return send_motorspeeds(speeds[0],speeds[1],speeds[2],speeds[3]) #returns motor data
+
+def send_motorspeeds(m1 = 0,m2 = 0,m3 = 0,thrower = 0): # send speeds to motors and return data
+    ser.write(struct.pack('<hhhHBH', m1, m2, m3, thrower, 0, 0xAAAA))
+    size = struct.calcsize('<hhhH')
+    data = ser.read(size)
+    values = struct.unpack('<hhhH',data)
+    return values #returns motor data
+
+    #linear_velocity = overall_speed * math.cos(direction - math.radians(wheel_angle))
+
+def combine_moves(move1, p1, move2, p2):
+    speed = [0,0,0,0]
+    for i,v in enumerate(move1):
+        speed[i]+= v*p1
+    for i,v in enumerate(move2):
+        speed[i]+= v*p2
+    return speed
+
+def move_omni(speed, angle): 
+    #linear_velocity = overall_speed * math.cos(direction - math.radians(wheel_angle))
+    
+    return [int(speed * math.cos(math.radians(angle) - math.radians(210))), int(speed * math.cos(math.radians(angle) - math.radians(300))), int(speed * math.cos(math.radians(angle) - math.radians(90))), 0]
+
+def rotate_omni(speed): 
+    return[speed,speed,speed,0]
+
+def rectify_speed(object, max_speed): 
+    mx = (max(object[0:len(object)-1]))
+    mx = max_speed/mx
+    object = [int(element * mx) for element in object]
+    return object
+
+def main(nearest_ball):
+    ser = None
+    try:
+        port='/dev/ttyACM0'
+        ser = serial.Serial(port, baudrate=115200, timeout=3)
+            send_ms(rectify_speed(combine_moves(move_omni(20, 0),1,rotate_omni(10),1),20))
+            sleep(0.05)
+    except Exception as e:
+        print(e)
+    if ser != None:
+        ser.close()
