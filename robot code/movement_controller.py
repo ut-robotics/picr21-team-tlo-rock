548106443bd108571a1bfb68125f91445ba867af
robot <robot@robot.robot>
2021-10-14 16:27:58 +0300

    distance measuring working, ball following working

--- robot code/movement_controller.py
+++ robot code/movement_controller.py
@@ -33,7 +33,7 @@ def move_omni(speed, angle): #generate movement vector with direction and speed
 
     #linear_velocity = overall_speed * math.cos(direction - math.radians(wheel_angle))
     
-    return [int(speed * math.cos(math.radians(angle) - math.radians(210))), int(speed * math.cos(math.radians(angle) - math.radians(300))), int(speed * math.cos(math.radians(angle) - math.radians(90))), 0]
+    return [int(speed * math.cos(math.radians(angle) - math.radians(210))), int(speed * math.cos(math.radians(angle) - math.radians(330))), int(speed * math.cos(math.radians(angle) - math.radians(90))), 0]
 
 def rotate_omni(speed): # generate rotation vector with speed
     return[speed,speed,speed,0]
@@ -42,29 +42,30 @@ def rectify_speed(object, max_speed): #changes the biggest wheel speed to be the
     obj = object[0:len(object)-1]
     mx = 0
     for i in obj:
-        if abs(i)> mx:
+        if abs(i) > mx:
             mx = abs(i)
     mx = max_speed/mx
     object = [int(element * mx) for element in object]
     return object # returns the correctedmovement vector
 
 def main(nearest_ball, running):# main function of movement controller
-    robot_speed = 30 # robots speed
+    robot_speed = 100 # robots speed
     ser = None    #create serial connection
     try:
-        port='/dev/ttyACM0'
+        port='/dev/ttyACM1'
         ser = serial.Serial(port, baudrate=115200, timeout=3)
 
         last_ball = []
         stop_counter = 0
 
         while True:
+
             #print(running.value)
             #print(nearest_ball[0], nearest_ball[1])
             if running.value == 0:# stop if no longer runing
                 break
             if nearest_ball[0] != 0: # failsafe
-                if nearest_ball[1] < 500:
+                if nearest_ball[1] < 400:
                     send_ms(ser, stop())
                 else:
                     if nearest_ball == last_ball:
@@ -77,9 +78,9 @@ def main(nearest_ball, running):# main function of movement controller
                     
                     p_val = (nearest_ball[0]-320)/320 #p controller
                     #print(p_val)
-                    movement_vector = combine_moves(move_omni(100, 0),move_omni(p_val*30, -90)) #calculate movement vector trying to center the ball
-                    #turning_vector = combine_moves (movement_vector, rotate_omni(p_val*10)) # calculate turning vector trying to turn toward the ball
-                    wheel_speeds = rectify_speed(movement_vector , robot_speed) #set movement speed
+                    movement_vector = combine_moves(move_omni(90, 0),move_omni(p_val*10, -90)) #calculate movement vector trying to center the ball
+                    turning_vector = combine_moves (movement_vector, rotate_omni(p_val*50)) # calculate turning vector trying to turn toward the ball
+                    wheel_speeds = rectify_speed(turning_vector , robot_speed) #set movement speed
                     motor_data = send_ms(ser,wheel_speeds) # execute movement
     except Exception as e:
         print(e)
