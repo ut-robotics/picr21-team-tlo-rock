bc93f6a462566ae54694476d2187584934628d0f
robot <robot@robot.robot>
2021-10-21 20:45:41 +0300

    tuning of manual controls

--- robot code/manual_control.py
+++ robot code/manual_control.py
@@ -11,8 +11,8 @@ def manualdrive(inputarray, control_state, speeds):
                 continue
 
             movement_vector = ml.combine_moves(ml.move_omni(100*(inputarray[1]-inputarray[5]), 0), ml.move_omni(100*(inputarray[4]-inputarray[3]), 90)) #calculate movement vector
-            turning_vector = ml.combine_moves (movement_vector, ml.rotate_omni(100*(inputarray[2]-inputarray[0]))) # calculate turning vector
-            wheel_speeds = ml.rectify_speed(turning_vector, 60) #set movement speed
+            turning_vector = ml.combine_moves (movement_vector, ml.rotate_omni(50*(inputarray[0]-inputarray[2]))) # calculate turning vector
+            wheel_speeds = ml.rectify_speed(turning_vector, 50) #set movement speed
             print(wheel_speeds)
             #print(inputarray[0],inputarray[1],inputarray[2],inputarray[3],inputarray[4],inputarray[5],inputarray[6])
             ml.set_speed(speeds, wheel_speeds)
