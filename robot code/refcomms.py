1d4c8dfabb8667782f1dae17041cc38dd645e288
robot <robot@robot.robot>
2022-01-26 13:23:00 +0200

    referee debugged

--- robot code/refcomms.py
+++ robot code/refcomms.py
@@ -6,7 +6,7 @@ from enums import *
 async def refcommClient(robotState, attackingSide):
     
     robotName = 'TLOROCK'
-    connectTo = 'ws://192.168.3.220:8220'
+    connectTo = 'ws://192.168.3.220:8222'
 
     async with websockets.connect(connectTo, ping_interval=None) as websocket:
         while True:
@@ -19,11 +19,11 @@ async def refcommClient(robotState, attackingSide):
                         attackingSide = Side.blue
                     else:
                         attackingSide = Side.pink
-                    robotState = State.automatic
+                    robotState.value = State.automatic
                     print('Starting competition!')
 
                 elif refdict['signal'] == 'stop':
-                    robotState = State.remote
+                    robotState.value = State.remote
                     print('Stopping competition! Switching to manual control...')
             else:
                 print('Signal not directed at', robotName)
@@ -32,12 +32,7 @@ async def refcommClient(robotState, attackingSide):
 
 
 def refclient(state, attacking):
-    while True:
-        try:
-            asyncio.run(refcommClient(state, attacking))
-        except:
-            print('Ref not reached!')
-            pass
+    asyncio.run(refcommClient(state, attacking))
 
 if __name__ == '__main__':
     refclient(5, 5)
